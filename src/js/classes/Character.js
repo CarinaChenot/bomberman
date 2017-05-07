939b491fb09d91227c685845ce4d0b94de88895a
NoeFalque <noe.falque@gmail.com>
2017-05-07 14:28:45 +0200

    :sparkles: Added speed control and multiplayer

--- src/js/classes/Character.js
+++ src/js/classes/Character.js
@@ -12,6 +12,7 @@ class  Character {
 		this.div
 		this.anim = {}
 		this.anim.step = 0;
+		this.direction = "none"
 		//init:
 		this.create()
 		this.controls()
@@ -25,65 +26,29 @@ class  Character {
 		document.querySelector(".game-container").appendChild(this.div)
 	}
 
-	move(dir){
-		const nextPos = {}
-		nextPos.x = this.pos.x
-		nextPos.y = this.pos.y
-		if(dir =="up"){
-			nextPos.y -= 1;
-		}
-		if(dir =="down"){
-			nextPos.y += 1;
-		}
-		if(dir =="left"){
-			nextPos.x -= 1;
-		}
-		if(dir =="right"){
-			nextPos.x += 1;
-		}
-		if( !map.map[nextPos.y][nextPos.x].solid ){ // if cell is free:
-			this.pos = nextPos
-			this.div.style.top 	= this.pos.y*map.cell_size + "px"
-			this.div.style.left = this.pos.x*map.cell_size + "px"
-		}
-		this.animate(dir)
-
-		let bonus = map.map[this.pos.y][this.pos.x].bonus
-		if(  bonus == "range" ){
-			map.map[this.pos.y][this.pos.x].bonus = undefined;
-			this.range++
-			setTimeout( () => {
-				this.range--
-			}, 5000)
-			console.log("New range: "+this.range)
-		}
-		if( bonus == "speed" ){
-			map.map[this.pos.y][this.pos.x].bonus
-			this.speed *= 1.2;
-			setTimeout( () => {
-				this.speed /= 1.2
-			}, 5000)
-			console.log("New speed: "+this.speed)
-		}
-	}
-
 	controls(){
-		window.addEventListener('keypress', (e) => {
-			if(e.keyCode==this.keys[0]){
-				this.move("up")
-			}
-			if(e.keyCode==this.keys[1]){
-				this.move("down")
-			}
-			if(e.keyCode==this.keys[2]){
-				this.move("left")
-			}
-			if(e.keyCode==this.keys[3]){
-				this.move("right")
-			}
-			if(e.keyCode==this.keys[4]){
+		let moving
+		window.addEventListener('keydown', (e) => {
+			if(e.keyCode==this.keys.bomb){ // bombs
 				this.dropBomb();
 			}
+			else { // directions
+				Object.keys(this.keys).forEach( (key) => {
+					if(e.keyCode==this.keys[key]){
+						if(this.direction != key) {
+							this.direction = key
+							this.move();
+							moving = setInterval( () => {
+								this.move();
+							}, 200 / this.speed)
+						}
+					}
+				} );
+			}
+		}, false)
+		window.addEventListener('keyup', (e) => {
+			this.direction = "none"
+			clearInterval(moving)
 		}, false)
 		// document.querySelector(".top").addEventListener('click', (e) => {
 		// 	this.move("up")
@@ -99,19 +64,54 @@ class  Character {
 		// }, false)
 	}
 
-	animate(dir){
+	move() {
+		const nextPos = {}
+		nextPos.x = this.pos.x
+		nextPos.y = this.pos.y
+		if(this.direction == "up") 		nextPos.y -= 1
+		if(this.direction == "down") 	nextPos.y += 1
+		if(this.direction == "left") 	nextPos.x -= 1
+		if(this.direction == "right") 	nextPos.x += 1
+
+		if( !map.map[nextPos.y][nextPos.x].solid ){ // if cell is free:
+			this.pos = nextPos
+			this.div.style.top 	= this.pos.y*map.cell_size + "px"
+			this.div.style.left = this.pos.x*map.cell_size + "px"
+		}
+		this.animate()
+
+		let bonus = map.map[this.pos.y][this.pos.x].bonus
+		if(  bonus == "range" ){
+			map.map[this.pos.y][this.pos.x].bonus = undefined;
+			this.range++
+			setTimeout( () => {
+				this.range--
+			}, 5000)
+			console.log("New range: "+this.range)
+		}
+		if( bonus == "speed" ){
+			map.map[this.pos.y][this.pos.x].bonus
+			this.speed *= 1.1;
+			this.div.style.transitionDuration = 0.2 / this.speed +"s"
+			setTimeout( () => {
+				this.speed /= 1.1
+			}, 5000)
+			console.log("New speed: "+this.speed)
+		}
+	}
+
+	animate(){
 		this.anim.step >= 2 ? this.anim.step=0 : this.anim.step++
 		clearInterval(this.anim.timeout)
 		let dirOffset;
-		switch (dir) {
-			case "left": dirOffset = 0; break
-			case "down": dirOffset = 3; break
-			case "up": dirOffset = 6; break
-			default: dirOffset = 9; break
+		switch (this.direction) {
+			case "left"	: dirOffset = 0; break
+			case "down"	: dirOffset = 3; break
+			case "up"	: dirOffset = 6; break
+			default		: dirOffset = 9; break
 		}
 		this.anim.timeout = setTimeout( () => { // stand without walking
-			this.anim.step = 1;
-			this.div.style.backgroundPosition = "-"+ (dirOffset + this.anim.step) * 20 +"px 0"
+			this.div.style.backgroundPosition = "-"+ (dirOffset + 1) * 20 +"px 0"
 		}, 100)
 		this.div.style.backgroundPosition = "-"+  (dirOffset + this.anim.step) * 20  +"px 0"
 	}
@@ -121,6 +121,8 @@ class  Character {
 	}
 }
 
-//                                            pos  |  z    s    q    d    b
-//                                            x  y | up  down left right bomb
-var character1 = new Character("Character 1", 1, 1, [122, 115, 113, 100, 98], true)
+//                                            pos  |    z          s         q         d       space
+var character1 = new Character("Character 1", 1, 1, {up: 90,  down: 83, left: 81, right: 68, bomb: 32})
+
+//                                            pos  |         a r r o w    k e y s              enter
+var character2 = new Character("Character 2", 5, 1, {up: 38,  down: 40, left: 37, right: 39, bomb: 13})
