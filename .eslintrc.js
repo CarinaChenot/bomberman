ef5a3f0a178e0de5f6227c9e158e0427bc3c1977
CarinaChenot <mllecarinach@gmail.com>
2017-05-08 02:03:58 +0200

    :wrench: Added ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+// run eslint --fix src/** to fix all
+
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es6: true,
+    node: true
+  },
+  extends: 'eslint:recommended',
+  // extends: 'standard',
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module'
+  },
+  'rules': {
+    'indent': ['error', 2],
+    'space-before-blocks': 'error',
+    'linebreak-style': ['error', 'unix'],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'never'],
+    'space-infix-ops': ['error', {'int32Hint': false}],
+    'keyword-spacing': ['error', { 'before': true, 'after': true }],
+    'no-multi-spaces': 0,
+    'space-before-function-paren': ['error', 'never'],
+    'space-in-parens': ['error', 'never'],
+    'eqeqeq': ['error', 'always'],
+    'brace-style': ['error', '1tbs', { 'allowSingleLine': true }],
+    'spaced-comment': ['error', 'always'],
+    'padded-blocks': ['error', 'never'],
+    'no-multiple-empty-lines': 'error',
+    'no-unused-expressions': 'error',
+    'comma-spacing': ['error', { 'before': false, 'after': true }],
+    'key-spacing': ["error", { 'mode': 'strict' }]
+  }
+}
