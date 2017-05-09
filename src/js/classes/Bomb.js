e1d2f886966cf4e5a95ff9277034869c12802b80
CarinaChenot <mllecarinach@gmail.com>
2017-05-09 20:35:28 +0200

    :bug: Bombs fix

--- src/js/classes/Bomb.js
+++ src/js/classes/Bomb.js
@@ -7,65 +7,40 @@ class Bomb {
     this.exploding = false
     this.cell      = map.map[this.pos.y][this.pos.x]
     this.el        = document.createElement('div')
-    this.delay     = 0
+    this.delay     = 2000
     this.init()
   }
   init() {
-    this.render()
+    this.cell.content.classList.add('bomb')
     setTimeout(() => { this.explode() }, this.delay)
-  }
-  render() {
-    this.el.classList.add('bomb')
-    this.el.style.transform = 'translateX(' + this.pos.x + '0px) translateY(' + this.pos.y + '0px)'
-    document.getElementById('app').appendChild(this.el)
   }
   explode() {
     this.exploding = true
 
-    for (var i = 0; i <= this.range; i++) {
+    let fire = []
+    fire.push(map.map[this.pos.y][this.pos.x])
 
-      // Retrieve cells
-      let up    = map.map[this.pos.y + Math.abs(i) * -1][this.pos.x]
-      let down  = map.map[this.pos.y + Math.abs(i)][this.pos.x]
-      let left  = map.map[this.pos.y][this.pos.x + Math.abs(i) * -1]
-      let right = map.map[this.pos.y][this.pos.x + Math.abs(i)]
-      const toCheck = [up, down, left, right]
-
-      toCheck.forEach((elem) => {
-        elem.content.classList.add('fire')
+    // Retrieve cells where to add fire
+    for (var i = 1; i <= this.range; i++) {
+      let axis = [
+        map.map[this.pos.y + Math.abs(i) * -1][this.pos.x],
+        map.map[this.pos.y + Math.abs(i)][this.pos.x],
+        map.map[this.pos.y][this.pos.x + Math.abs(i) * -1],
+        map.map[this.pos.y][this.pos.x + Math.abs(i)]
+      ]
+      axis.forEach((elem) => {
+        if (!elem.solid) { fire.push(elem) }
       })
-
-      let position = 0
-      let spriteRow = 62
-      let frame = setInterval(() => { requestAnimationFrame(explodeRendering.bind(this))}, 100)
-
-      function explodeRendering() {
-        // Stop condition
-        if (position > 224) {
-          toCheck.forEach((elem) => { elem.content.classList.remove('fire') })
-          this.reset(frame)
-          return
-        }
-
-        if (i === this.range) {
-          toCheck.forEach((elem) => { elem.content.style.backgroundPositionY = '62px' })
-        }
-
-        if (i === 0) {
-          spriteRow = 0
-        }
-        left.content.style.backgroundPosition = Math.abs(position) * -1 + 'px ' + spriteRow + 'px'
-        right.content.style.backgroundPosition = Math.abs(position) * -1 + 'px ' + spriteRow + 'px'
-        up.content.style.backgroundPosition = Math.abs(position) * -1 + 'px ' + spriteRow + 'px'
-        up.content.style.transform = 'rotate(90deg)'
-        down.content.style.transform = 'rotate(90deg)'
-        down.content.style.backgroundPosition = Math.abs(position) * -1 + 'px ' + spriteRow + 'px'
-        position += 32 // Increment to next frame
-      }
     }
+
+    // Spread fire
+    fire.forEach((elem) => { elem.content.classList = 'content fire' })
+
+    // Reset
+    setTimeout(() => { this.reset(fire) }, 500)
   }
-  reset(frame) {
-    clearInterval(frame)
+  reset(fire) {
+    fire.forEach((elem) => { elem.content.classList.remove('fire') })
     this.isAlive = false
     this.el.remove()
     this.cell.bomb = null
@@ -81,31 +56,3 @@ function newBomb() {
   map.map[pos.y][pos.x].bomb = new Bomb(1, 1, pos)
 }
 // newBomb()
-
-
-
-/*** Notes
-
-return bombList.push(new Bomb(1, 2, pos))
-
-// To create bomb on the Player class
-return bombList.push(new Bomb(this.player, this.range, this.pos))
-
-// List of unexploded bombs on the map
-let bombList = []
-
-updateList() {
-  // Remove exploded bombs from list
-  bombList = bombList.filter(function(elem) {
-    return elem.isAlive
-  })
-}
-
-resetLethality(cell) {
-  cell.lethal = false
-}
-
-// Call reset after 0.5s of exlosion
-setTimeout(() => { this.reset() }, 500)
-
-***/
