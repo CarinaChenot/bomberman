401e5030398a43602917c27ccaee01d600d8395f
Paul-maire <paul.maire@hetic.net>
2017-05-19 11:09:17 +0200

    Fix IA goToTarget

--- src/js/classes/IA.js
+++ src/js/classes/IA.js
@@ -14,19 +14,16 @@ class IA extends Character {
     document.querySelector('.game-container').appendChild(this.div)
   }
 
-  controls() {
-
-  }
-
   goToTarget() {
     setInterval(() => {
-      characters[0].pos.x - this.pos.x < 0 ? this.pos.x -- : this.pos.x ++
-      characters[0].pos.y - this.pos.y < 0 ? this.pos.y -- : this.pos.y ++
-      super.move()
-      console.log(this);
-    }, 100)
+      if (characters[0]) {
+        characters[0].pos.x - this.pos.x < 0 ? this.pos.x -- : this.pos.x ++
+        characters[0].pos.y - this.pos.y < 0 ? this.pos.y -- : this.pos.y ++
+        super.move()
+      }
+    }, 300)
   }
 }
 
-// var ia = new IA(3, {x: 5, y: 5}, {up: 38,  down: 40, left: 37, right: 39, bomb: 13})
-// ia.goToTarget()
+var ia = new IA(3, {x: 5, y: 5}, {up: 38,  down: 40, left: 37, right: 39, bomb: 14})
+ia.goToTarget()
