d4cccdca86c298546add8b487485a69389eb3626
thibobbs <thibaut.renard@hetic.net>
2017-05-22 16:30:04 +0200

    :lipstick: add menu

---
+++ src/js/classes/Settings.js
@@ -0,0 +1,50 @@
+// Settings class
+class Settings {
+  constructor(timer = "1'", map_size = 'medium', nb_players = 2, nb_AI = 2) {
+    this.timer = parseInt(timer);
+    if (map_size === 'small') {
+      this.map_size = [10, 10]
+    } else if (map_size === 'medium') {
+      this.map_size = [15, 15]
+    } else if (map_size === 'big') {
+      this.map_size = [20, 20]
+    }
+    this.nb_players = nb_players
+    this.nb_AI = nb_AI
+    this.game_status = true
+  }
+}
+
+const settings = {}
+settings.div = document.querySelector('.settings')
+settings.button = settings.div.querySelector('.start')
+settings.button.onclick = function () {
+  let timer = settings.div.querySelector('.timer button.selected').innerHTML
+  let map_size = settings.div.querySelector('.size button.selected').innerHTML
+  let nb_players = settings.div.querySelector('.players button.selected').innerHTML
+  let nb_AI = settings.div.querySelector('.ai button.selected').innerHTML
+  const game = new Settings(timer, map_size, nb_players, nb_AI)
+  console.log(game)
+}
+settings.timer_div = settings.div.querySelectorAll('.timer button')
+settings.size_div = settings.div.querySelectorAll('.size button')
+settings.players_div = settings.div.querySelectorAll('.players button')
+settings.ai_div = settings.div.querySelectorAll('.ai button')
+
+settings.select = (nodes) => {
+  for (let i = 0; i < nodes.length; i++) {
+    nodes[i].onclick = function () {
+      const _nodes = this.parentElement.querySelectorAll('button')
+      for (let y = 0; y < _nodes.length; y++) {
+        if (_nodes[y].className.includes('selected')) {
+          _nodes[y].className = ''
+        }
+      }
+      this.className = ' selected'
+    }
+  }
+}
+settings.select(settings.timer_div)
+settings.select(settings.size_div)
+settings.select(settings.players_div)
+settings.select(settings.ai_div)
