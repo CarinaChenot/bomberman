2e933c5afe8ed2e678a7545b346bed3d5ab50f8b
BaptisteVillain <BVillain92@gmail.com>
2017-05-29 14:06:31 +0200

    :rocket: change main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,21 @@
-// const socket = new Live()
-// socket.start()
+function startGame(settings) {
+  resetGame()
+  map.size = settings.map_size
+  map.setWidth()
+  map.generate()
+  for (let i = 0; i < settings.nb_players ; i++) {
+    characters[characters.length] = new Character(i, {x: map.spawn_point[i][0], y: map.spawn_point[i][1]}, {up: settings.player_control[i][0],  down: settings.player_control[i][1], left: settings.player_control[i][2], right: settings.player_control[i][3], bomb: settings.player_control[i][4]})
+  }
+
+
+  for (let j = settings.nb_players; j < settings.nb_AI + settings.nb_players ; j++){
+    if (characters.length < 4) {
+      characters[characters.length] = new IA(j, {x: map.spawn_point[j][0], y: map.spawn_point[j][1] }, {up: 380,  down: 400, left: 370, right: 390, bomb: 140})
+      characters[characters.length - 1].goToTarget()
+    }
+  }
+}
+
+function resetGame(){
+  characters = []
+}
